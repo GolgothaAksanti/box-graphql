496558d8dd4b911c2beb0c33994c19051ab3078e
GolgothaAksanti <38277247+GolgothaAksanti@users.noreply.github.com>
2021-04-20 16:39:55 +0300

    ch(Resolvers): Apollo server resolvers

--- index.js
+++ index.js
@@ -8,25 +8,48 @@ app.use(cors());
 
 const schema = gql`
   type Query {
+    users: [User!]
+    user(id: ID!): User
     me: User
   }
 
   type User {
+    id: ID!
     username: String!
   }
 `;
 
+const users = {
+  1: {
+    id: '1',
+    username: 'Altare Aksanti',
+  },
+  2: {
+    id: '2',
+    username: 'Mapendo Celestin',
+  },
+};
+
+const me = users[1];
+
 const resolvers = {
   Query: {
-    me: () => ({
-      username: 'Golgotha Aksanti',
-    }),
+    users: () => Object.values(users),
+    user: (parent, { id }) => users[id],
+    me: (parent, args, { me }) => me,
+  },
+
+  User: {
+    username: (user) => `${user.firstname} ${user.lastname}`,
   },
 };
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    me: users[1],
+  },
 });
 
 server.applyMiddleware({
