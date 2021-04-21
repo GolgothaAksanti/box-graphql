0594c28e5903a91bad1c4335b63e503006602738
GolgothaAksanti <38277247+GolgothaAksanti@users.noreply.github.com>
2021-04-21 14:38:41 +0300

    ch(type relationship): Relationship between 2 types

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ const schema = gql`
     users: [User!]
     user(id: ID!): User
     me: User
+
+    messages: [Message!]!
+    message(id: ID!): Message!
   }
 
   type User {
     id: ID!
     username: String!
+    messages: [Message!]
+  }
+
+  type Message {
+    id: ID!
+    text: String!
+    user: User!
   }
 `;
 
@@ -23,24 +33,45 @@ const users = {
   1: {
     id: '1',
     username: 'Altare Aksanti',
+    messageIds: [1],
   },
   2: {
     id: '2',
     username: 'Mapendo Celestin',
+    messageId: [2],
   },
 };
 
 const me = users[1];
+
+const messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By Wolrd',
+    userId: '2',
+  },
+};
 
 const resolvers = {
   Query: {
     users: () => Object.values(users),
     user: (parent, { id }) => users[id],
     me: (parent, args, { me }) => me,
+    messages: () => Object.values(messages),
+    message: (parent, { id }) => messages[id],
   },
 
   User: {
-    username: (user) => `${user.firstname} ${user.lastname}`,
+    messages: (user) => Object.values(messages).filter((message) => message.userId === user.id),
+  },
+
+  Message: {
+    user: (message) => users[message.userId],
   },
 };
 
